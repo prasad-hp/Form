dcd92cc16cdc864c7f5c2fbb7943f6444f90ecb8
Prasad Hp <prasadhp@outlook.in>
2024-07-02 21:09:11 +0530

    Removed python folder

--- Backend-Python/myproject/myproject/urls.py
+++
@@ -1,8 +0,0 @@
-# myapp/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('data/', views.data_handler, name='data_handler'),
-    # Define other URL patterns if needed
-]
